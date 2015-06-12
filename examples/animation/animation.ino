d9c47284e264fa8c65d19d6a8d768c5bde90a003
Joeri Gantois <info@JO3RI.be>
2015-06-12 11:59:19 +0200

    Update animation.ino

--- examples/animation/animation.ino
+++ examples/animation/animation.ino
@@ -1,8 +1,22 @@
+/*
+  Simple Animation example of a sprite
+  Written By Joeri http://www.JO3RI.be , 12 June 2015
+*/
 
+#include <SPI.h>
+#include <EEPROM.h>
+#include <Arduboy.h>
+#include <ArduboyExtra.h>
+#include <sprites.h>
+#include "bitmaps.h"
+
+Arduboy arduboy;
+Sprites sprites (arduboy);
 
 void setup() {
-  // put your setup code here, to run once:
-
+  arduboy.start();
+  arduboy.clearDisplay();
+  arduboy.display();
 }
 
 void loop() {
